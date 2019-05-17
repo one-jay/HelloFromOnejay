60298ce5d6128a51a2da8f6321e9178290684843
sekijohn10 <50596878+sekijohn10@users.noreply.github.com>
2019-05-17 17:27:01 -0600

    Intellij issues.

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from Clayton!");
         System.out.println("Hello Clayton, this is David");
+        System.out.println("Hello Clayton, I have to push this in the GitHub Desktop.")
     }
 }
