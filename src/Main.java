9f196156c02318e2095f8c239d38913f45ae3990
davidrgmcb <davidrgmcburney@gmail.com>
2019-05-16 20:05:44 -0700

    Greetings from David

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello from Clayton!");
+        System.out.println("Hello Clayton, this is David");
     }
 }
