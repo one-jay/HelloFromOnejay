39057650b32bfc745389def62e405f8480744ddf
Clayton Thomas <onejay@outlook.com>
2019-05-17 20:53:43 -0700

    default changelist

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from Clayton!");
         System.out.println("Hello Clayton, this is David");
-        System.out.println("Hello Clayton, I have to push this in the GitHub Desktop.")
+        System.out.println("Hello Clayton, I have to push this in the GitHub Desktop.");
+        System.out.println("Cool, so it looks like I was able to get updates from David and Tim, " +
+                "and then pull those changes and add my own");
     }
 }
